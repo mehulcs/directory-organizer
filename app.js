c844a3143df4eb8e78aa97eefd73184b3544096c
mehulcs <mehul.becs@gmail.com>
2019-10-09 18:44:42 +0530

    Directory Organizer - Init

---
+++ app.js
@@ -0,0 +1,50 @@
+const fs = require("fs-extra");
+const path = require("path");
+const chokidar = require("chokidar");
+
+const config = require("./config.json");
+
+config.forEach(module => {
+  const modulePath = module.directory;
+  console.log("Monitoring " + modulePath);
+
+  module.files.forEach(type => {
+    createIfNotExist(path.join(module.directory + type.directory));
+  });
+
+  chokidar.watch(module.directory).on("add", filePath => {
+    const fileExtension = path.extname(filePath).substr(1);
+    const fileName = path.basename(filePath);
+
+    module.files.forEach(async type => {
+      if (type.fileTypes.includes(fileExtension)) {
+        try {
+          moveFile(filePath, path.join(modulePath, type.directory, fileName));
+        } catch (error) {
+          console.log("error: ", error);
+        }
+        console.log(`Moved ${fileName} to ${type.directory} directory`);
+      }
+    });
+  });
+});
+
+/**
+ * Create directory at path if path is not directory
+ * @param {String} path
+ */
+function createIfNotExist(path) {
+  if (!fs.existsSync(path)) {
+    console.log("Creating directory : ", path);
+    fs.mkdirSync(path);
+  }
+}
+
+/**
+ *
+ * @param {String} fromPath - full path of old file
+ * @param {String} toPath - full path of new file
+ */
+async function moveFile(fromPath, toPath) {
+  await fs.rename(fromPath, toPath);
+}
